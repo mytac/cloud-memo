5cb8d6bab3b701ee1c0bd10c4a2387b44b0f5c87
张君卓 <my_tac@126.com>
2018-02-08 17:12:10 +0800

    使用airbnb规则

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,29 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+    extends: ['airbnb']
 };
