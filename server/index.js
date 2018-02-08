02aeaa94020e32dd727a90da34a8864df27b278e
张君卓 <my_tac@126.com>
2018-02-08 17:25:22 +0800

    处理404错误

--- server/index.js
+++ server/index.js
@@ -17,6 +17,11 @@ app.get('/findByLabel', (req, res) => {
   res.send('findByLabel!!');
 });
 
+// 404
+app.use((req, res) => {
+  res.status(404).send('404 not found');
+});
+
 const server = app.listen(8888, () => {
   const host = server.address().address;
   const { port } = server.address();
