d461cadf377e81b5c6f0b721ddf6f46049ce19ec
张君卓 <my_tac@126.com>
2018-02-08 17:53:37 +0800

    创建路由

--- server/index.js
+++ server/index.js
@@ -1,25 +1,32 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
 // 上传文章
-app.get('/upload', (req, res) => {
+router.get('/upload', (req, res) => {
   res.send('uploaded!!');
 });
 
 // 删除文章
-app.get('/delete', (req, res) => {
+router.get('/delete', (req, res) => {
   res.send('delete!!');
 });
 
 // 按分类查找
-app.get('/findByLabel', (req, res) => {
+router.get('/findByLabel', (req, res) => {
   res.send('findByLabel!!');
 });
 
 // 404
-app.use((req, res) => {
+router.use((req, res) => {
   res.status(404).send('404 not found');
+});
+
+// 500
+router.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 const server = app.listen(8888, () => {
