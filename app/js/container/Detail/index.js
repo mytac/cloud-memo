395dcf44013cbc63bc528d601c4c54fe7db6513d
张君卓 <my_tac@126.com>
2018-03-07 13:50:26 +0800

    加入动画，过渡换行闪烁

--- app/js/container/Detail/index.js
+++ app/js/container/Detail/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, TextInput, StyleSheet } from 'react-native';
+import { ScrollView, TextInput, StyleSheet, Animated } from 'react-native';
 import { px2dp } from '../../utils/px2dp';
 
 
@@ -7,28 +7,34 @@ export default class Detail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputBoxHeight: 0,
+      inputBoxHeight: new Animated.Value(),
     };
     this.onChange = this.onChange.bind(this);
   }
 
   onChange(event) {
     const contentSize = event.nativeEvent.contentSize.height;
-    this.setState({
-      inputBoxHeight: contentSize,
-    });
+    Animated.timing(
+      this.state.inputBoxHeight,
+      {
+        toValue: contentSize,
+      },
+    ).start();
   }
 
   render() {
     const { inputBoxHeight } = this.state;
     return (
       <ScrollView style={{ flex: 1 }}>
-        <TextInput
-          style={[{ height: inputBoxHeight }, style.inputBox]}
-          multiline // 代表可以输入多行
-          defaultValue="可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行"
-          onContentSizeChange={this.onChange}
-        />
+        <Animated.View style={{ height: inputBoxHeight }}>
+          <TextInput
+            style={[{ flex: 1 }, style.inputBox]}
+            multiline // 代表可以输入多行
+            defaultValue="可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行"
+            onContentSizeChange={this.onChange}
+            underlineColorAndroid="transparent"
+          />
+        </Animated.View>
       </ScrollView>
     );
   }
@@ -37,7 +43,6 @@ export default class Detail extends Component {
 
 const style = StyleSheet.create({
   inputBox: {
-    borderWidth: 1,
     fontSize: px2dp(45),
   },
 });
