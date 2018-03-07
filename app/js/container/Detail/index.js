b9f82ab701c36b14161c93667bb6d73a62cba660
张君卓 <my_tac@126.com>
2018-03-07 11:08:41 +0800

    添加Detail组件，实现编辑时自动更改行高

--- app/js/container/Detail/index.js
+++ app/js/container/Detail/index.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, TextInput, StyleSheet } from 'react-native';
+import { px2dp } from '../../utils/px2dp';
 
 
 export default class Detail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputBoxHeight: 0,
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    const contentSize = event.nativeEvent.contentSize.height;
+    this.setState({
+      inputBoxHeight: contentSize,
+    });
+  }
+
   render() {
+    const { inputBoxHeight } = this.state;
     return (
       <ScrollView style={{ flex: 1 }}>
-        <Text>Detail</Text>
+        <TextInput
+          style={[{ height: inputBoxHeight }, style.inputBox]}
+          multiline // 代表可以输入多行
+          defaultValue="可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行"
+          onContentSizeChange={this.onChange}
+        />
       </ScrollView>
     );
   }
 }
+
+
+const style = StyleSheet.create({
+  inputBox: {
+    borderWidth: 1,
+    fontSize: px2dp(45),
+  },
+});
