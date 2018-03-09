1180cc547a7f0254f6c251fa6bebf2e18d2a49c8
张君卓 <my_tac@126.com>
2018-03-09 15:03:25 +0800

    添加goto 跳转路由事件

--- app/js/container/List/index.js
+++ app/js/container/List/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { ScrollView, View } from 'react-native';
 import Listitem from '../../component/Listitem';
 import AddBtn from '../../component/AddBtn';
@@ -7,17 +8,30 @@ import AddBtn from '../../component/AddBtn';
 const colorSet = ['#def2ff', '#bfeabe', '#f2d8c6', '#f0efb0'];
 
 export default class List extends Component {
+  constructor(props) {
+    super(props);
+    this.goto = this.goto.bind(this);
+  }
+
+  goto(tar) {
+    this.props.navigation.navigate(tar);
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <AddBtn />
+        <AddBtn onPress={() => this.goto('Detail')} />
         <ScrollView style={{ flex: 1 }}>
-          {mockData.map((data, idx) => <Listitem msg={data} color={colorSet[idx % 4]} onPress={() => this.props.navigation.navigate('Detail')} />)}
+          {mockData.map((data, idx) => <Listitem msg={data} color={colorSet[idx % 4]} onPress={() => this.goto('Detail')} />)}
         </ScrollView>
       </View>
     );
   }
 }
+
+List.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
 
 const mockData = [
   'test',
