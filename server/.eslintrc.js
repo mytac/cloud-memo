c747cd48ae95d1080ace5a9735f8c63cac43ac26
张君卓 <my_tac@126.com>
2018-03-09 14:16:48 +0800

    更改eslint规则

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
         "no-use-before-define": 0,
         'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
         'import/prefer-default-export': 0,
+        "react/forbid-prop-types":0
     }
 };
