fb83a81c788422890de3b2aa0fcb5420efe5002e
张君卓 <my_tac@126.com>
2018-03-14 13:52:31 +0800

    加上分页的api修改findDocuments方法

--- server/mongo/findByLabel.js
+++ server/mongo/findByLabel.js
@@ -3,10 +3,13 @@ const { createError } = require('../lib/createError');
 const { createResult } = require('../lib/createResult');
 const errors = require('../constants/errors');
 
-const findDocuments = (db, data = {}, collectionName = articleCollection) =>
+const findDocuments = (
+  db, data = {}, skipNum = 0, limit = 10,
+  collectionName = articleCollection,
+) =>
   new Promise((resolve, reject) => {
     const collection = db.collection(collectionName);
-    collection.find(data).toArray((err, docs) => {
+    collection.find(data).skip(skipNum).limit(limit).toArray((err, docs) => {
       if (err) {
         reject(createError(errors.HANDLE_DB_FAILED));
       } else {
