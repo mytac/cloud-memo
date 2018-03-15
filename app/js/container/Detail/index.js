52459545c067640cf35f09475be885b386e54d7c
张君卓 <my_tac@126.com>
2018-03-15 15:10:56 +0800

    两个路由间的数据存取

--- app/js/container/Detail/index.js
+++ app/js/container/Detail/index.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { ScrollView, TextInput, StyleSheet, Animated, DeviceEventEmitter } from 'react-native';
 import { px2dp } from '../../utils/px2dp';
 import request from '../../utils/request';
 import { Toast } from '../../component/Toast/index';
+import { setArticle } from '../../utils/localStorage';
 
 
 export default class Detail extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      /* 文本 */
+      title: '',
+      content: '',
+
       inputBoxHeight: new Animated.Value(),
     };
     this.onChange = this.onChange.bind(this);
@@ -27,6 +33,7 @@ export default class Detail extends Component {
   }
 
   componentWillUnmount() {
+    this.updateArticle();
     this.deEmitter.remove();
   }
 
@@ -41,11 +48,16 @@ export default class Detail extends Component {
   }
 
   registEmitter() {
-    this.deEmitter = DeviceEventEmitter.addListener('uploading', (a) => {
-      console.log('uploading', a);
+    this.deEmitter = DeviceEventEmitter.addListener('uploading', () => {
       // 上传操作
       DeviceEventEmitter.emit('isUploaded', true);
     });
+  }
+
+  updateArticle() {
+    const { updateArticle, index } = this.props.navigation.state.params;
+    const { title, content } = this.state;
+    updateArticle(title, content, index);
   }
 
   render() {
@@ -54,11 +66,18 @@ export default class Detail extends Component {
       <ScrollView style={{ flex: 1 }}>
         <Animated.View style={{ height: inputBoxHeight }}>
           <TextInput
+            placeholder="标题"
+            underlineColorAndroid="transparent"
+            style={[{ flex: 1 }, style.inputBox]}
+            onChangeText={(text) => { this.setState({ title: text }); }}
+          />
+          <TextInput
+            placeholder="内容"
             style={[{ flex: 1 }, style.inputBox]}
             multiline // 代表可以输入多行
-            defaultValue="可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行可以输入多行"
             onContentSizeChange={this.onChange}
             underlineColorAndroid="transparent"
+            onChangeText={(text) => { this.setState({ content: text }); }}
           />
         </Animated.View>
       </ScrollView>
@@ -72,3 +91,7 @@ const style = StyleSheet.create({
     fontSize: px2dp(45),
   },
 });
+
+Detail.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
--- app/js/utils/localStorage.js
+++ app/js/utils/localStorage.js
@@ -1,31 +1,24 @@
 import { AsyncStorage } from 'react-native';
-/* eslint-disable no-console */
-async function getStorageData(key) {
+
+async function setArticles(articles) {
+  let status = false;
+  try {
+    await AsyncStorage.setItem('Articles', articles);
+    status = true;
+  } catch (e) {
+    throw e;
+  }
+  return status;
+}
+
+async function getArticles() {
   let value;
   try {
-    value = await AsyncStorage.getItem(key);
+    value = await AsyncStorage.getItem('Articles');
   } catch (e) {
     throw e;
   }
   return value;
 }
 
-async function getStorageAllData(keys) {
-  let data;
-  try {
-    data = await AsyncStorage.multiGet(keys);
-  } catch (e) {
-    throw e;
-  }
-  return data;
-}
-
-async function setStorage(arr) {
-  try {
-    await AsyncStorage.multiSet(arr);
-  } catch (e) {
-    throw e;
-  }
-}
-
-export { getStorageData, getStorageAllData, setStorage };
+export { getArticles, setArticles };
