2b7ee04d311ea2d9e2c53aa18c40a7921b789ab1
张君卓 <my_tac@126.com>
2018-04-18 15:23:03 +0800

    添加<Switch>组件控制夜间模式开关

--- app/js/container/NightModel/index.js
+++ app/js/container/NightModel/index.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import { Text, TextInput } from 'react-native';
+import { Text, TextInput, Switch, View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import Dialog from '../../component/Dialog';
+import { px2dp } from '../../utils/px2dp';
 
 export default class NightModel extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isOpen: true,
+    };
   }
 
 
   render() {
     const { isVisible, onClose } = this.props;
+    const { isOpen } = this.state;
     return (
       <Dialog
         title="夜间模式"
         isVisible={isVisible}
         onClose={onClose}
-      />
+      >
+        <Switch
+          style={styles.switchWrapper}
+          disabled={false}
+          onTintColor="green"
+          onValueChange={(val) => {
+              this.setState({
+              isOpen: val,
+          });
+          }}
+          value={isOpen}
+          tintColor="green"
+        />
+      </Dialog>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  switchWrapper: {
+    width: px2dp(100),
+  },
+});
