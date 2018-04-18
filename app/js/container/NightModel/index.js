54677fc045da309c446548033d9154e87aa3fc39
张君卓 <my_tac@126.com>
2018-04-18 15:56:17 +0800

    夜间模式调试

--- app/js/container/NightModel/index.js
+++ app/js/container/NightModel/index.js
@@ -1,46 +1,34 @@
 import React from 'react';
-import { Text, TextInput, Switch, View, StyleSheet } from 'react-native';
+import { Switch, View } from 'react-native';
 import PropTypes from 'prop-types';
 import Dialog from '../../component/Dialog';
 import { px2dp } from '../../utils/px2dp';
 
-export default class NightModel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: true,
-    };
-  }
-
-
-  render() {
-    const { isVisible, onClose } = this.props;
-    const { isOpen } = this.state;
-    return (
-      <Dialog
-        title="夜间模式"
-        isVisible={isVisible}
-        onClose={onClose}
-      >
+export default function NightModel({
+  isVisible, onClose, isNightModel, toggleSwitch,
+}) {
+  return (
+    <Dialog
+      title="夜间模式"
+      isVisible={isVisible}
+      onClose={onClose}
+    >
+      <View style={{ width: px2dp(100) }}>
         <Switch
-          style={styles.switchWrapper}
           disabled={false}
-          onTintColor="green"
           onValueChange={(val) => {
-              this.setState({
-              isOpen: val,
-          });
+              toggleSwitch(val);
           }}
-          value={isOpen}
-          tintColor="green"
+          value={isNightModel}
         />
-      </Dialog>
-    );
-  }
+      </View>
+    </Dialog>
+  );
 }
 
-const styles = StyleSheet.create({
-  switchWrapper: {
-    width: px2dp(100),
-  },
-});
+NightModel.propTypes = {
+  isVisible: PropTypes.bool.isRequired,
+  isNightModel: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  toggleSwitch: PropTypes.func.isRequired,
+};
